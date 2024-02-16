225c445eccc2a49904a9e1a84c52b770885324d9
Mohammadreza Khalilikhorram <mhmmdrz1999@gmail.com>
2024-02-17 00:05:15 +0330

    -remove index.ts in utils -update input.ts config -get class dynamically in input.vue -update content in tailwind.config.ts to scan ui.config folders and files -update useUi function

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,20 @@
 import type {Config} from 'tailwindcss'
 
 export default {
-  content: [],
+  content: [
+    `./components/**/*.{vue,js,ts}`,
+    `./layouts/**/*.vue`,
+    `./pages/**/*.vue`,
+    `./composables/**/*.{js,ts}`,
+    `./plugins/**/*.{js,ts}`,
+    `./utils/**/*.{js,ts}`,
+    `./ui.config/**/*.{js,ts}`,
+    `./App.{js,ts,vue}`,
+    `./app.{js,ts,vue}`,
+    `./Error.{js,ts,vue}`,
+    `./error.{js,ts,vue}`,
+    `./app.config.{js,ts}`
+  ],
   theme: {
     extend: {
       fontFamily: {
