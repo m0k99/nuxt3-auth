6b5817fc8e95074dc76a89321c78d5f720f9ba10
Mohammadreza Khalilikhorram <mhmmdrz1999@gmail.com>
2024-02-19 19:50:23 +0330

    -change background color and opacity

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <AppMainContainer>
     <div class="w-dvw h-dvh p-4 flex items-center justify-center">
       <div
-        class="max-w-[1200px] w-full h-5/6 overflow-auto md:rounded-[80px] bg-white backdrop-blur-[100px] bg-opacity-30 rounded-3xl grid sm:grid-cols-2">
+        class="max-w-[1200px] w-full h-5/6 overflow-auto md:rounded-[80px] bg-white backdrop-blur-[100px] bg-opacity-10 rounded-3xl grid sm:grid-cols-2">
         <AppForm/>
         <div class="text-red-400 sm:block hidden">
           grid
