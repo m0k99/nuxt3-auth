4ad800fe883ff2baee65b769916fa15b32160780
Mohammadreza Khalilikhorram <mhmmdrz1999@gmail.com>
2024-02-21 00:24:57 +0330

    -change input style -decrease slot icon size to 20px

--- ui.config/input.ts
+++ ui.config/input.ts
@@ -1,8 +1,8 @@
 export default {
     wrapperClass: 'relative',
-    base: 'relative block w-full text-gray-600 disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none focus:border-gray-300 border-2',
+    base: 'relative bg-white block w-full text-gray-600 disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none focus:border-gray-300 border-2',
     rounded: 'rounded-full',
-    placeholder: 'placeholder-gray-400 dark:placeholder-gray-500',
-    padding: 'py-3.5 px-8',
+    placeholder: 'placeholder-gray-400',
+    padding: 'py-3 pl-8 pr-15',
     transition: 'transition ease-in-out duration-150'
 }
