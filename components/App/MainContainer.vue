5fc8ce0a26e396ab239ec989eccf520b1f4fe3d0
Mohammadreza Khalilikhorram <mhmmdrz1999@gmail.com>
2024-02-24 12:21:36 +0330

    -change container background

--- components/App/MainContainer.vue
+++ components/App/MainContainer.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <div class="w-dvw h-dvh fixed -z-20 bg-medium-blue">
-      <!--      <img src="/images/background.jpg" class="w-full h-full object-cover object-center" alt=""/>-->
-    </div>
+    <div class="container-background w-dvw h-dvh fixed -z-20"></div>
     <div class="w-dvw h-dvh fixed">
       <slot/>
     </div>
   </div>
 </template>
+<style lang="scss" scoped>
+.container-background {
+  background: #536976;
+  background: -webkit-linear-gradient(#292E49, #536976);
+  background: linear-gradient(#292E49, #536976);
+}
+</style>
