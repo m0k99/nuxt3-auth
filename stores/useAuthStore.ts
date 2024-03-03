2cfaba32b6394e542fa6fdf21b954e3f4fb9dbc9
Mohammadreza Khalilikhorram <mhmmdrz1999@gmail.com>
2024-03-03 16:37:45 +0330

    remove watch from login and error in user api

--- stores/useAuthStore.ts
+++ stores/useAuthStore.ts
@@ -10,15 +10,23 @@ type Credentials = {
     email: string;
     password: string;
 }
+
+type RegistrationInfo = {
+    name: string;
+    email: string;
+    password: string;
+    password_confirmation: string;
+}
 export const useAuthStore = defineStore('auth', () => {
     const user = ref<User | null>(null)
     const pending = ref(false)
     const isLoggedIn = computed(() => !!user.value)
 
     async function fetchUser() {
-        const {data,error} = await useApiFetch('/api/user')
+        const {data, error} = await useApiFetch('/api/user')
         console.log(error)
         user.value = data.value as User
+
     }
 
     async function login(credentials: Credentials) {
@@ -28,7 +36,6 @@ export const useAuthStore = defineStore('auth', () => {
 
         const login = await useApiFetch('/login', {
             method: 'POST',
-            watch: false,
             body: credentials
         })
 
@@ -39,5 +46,20 @@ export const useAuthStore = defineStore('auth', () => {
         return login
     }
 
-    return {user, pending, login, isLoggedIn, fetchUser}
+    async function register(info: RegistrationInfo) {
+        pending.value = true
+
+        await useApiFetch('/sanctum/csrf-cookie')
+
+
+        const register = await useApiFetch('/register', {
+            method: 'POST',
+            body: info
+        })
+        await fetchUser()
+        pending.value = false
+        return register
+    }
+
+    return {user, pending, login, isLoggedIn, fetchUser, register}
 })
