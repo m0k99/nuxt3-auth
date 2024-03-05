b01063577e895d4bc7d6a0eb92cf1eafef68d311
Mohammadreza Khalilikhorram <mhmmdrz1999@gmail.com>
2024-03-05 17:02:29 +0330

    add style on login user

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
   <AppMainContainer>
-    <div class="w-dvw h-dvh p-4 flex items-center justify-center">
+    <div class="w-dvw h-dvh flex items-center justify-center">
       <div
-        class="max-w-[1100px] w-full h-5/6 overflow-auto md:rounded-[80px] bg-white rounded-3xl grid md:grid-cols-2">
+        class="container-w w-full h-5/6 overflow-auto md:rounded-[80px] bg-white rounded-3xl grid md:grid-cols-2 transition-all ease-in-out duration-500"
+        :class="[loggedIn?'h-full md:rounded-none max-w-full':'max-w-[1100px]']"
+      >
         <AppForm/>
         <div class="text-red-400 md:block hidden p-5 w-full h-full overflow-hidden">
           <img
@@ -16,4 +18,13 @@
   </AppMainContainer>
 </template>
 <script setup lang="ts">
+import {useAuthStore} from "~/stores/useAuthStore";
+import {computed} from "vue";
+
+const auth = useAuthStore()
+const loggedIn = computed(() => auth.isLoggedIn)
 </script>
+
+<style lang="scss" scoped>
+//style
+</style>
