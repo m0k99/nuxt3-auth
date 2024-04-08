066d368f4bfa2faa3687ec1a81c0e118896c0c8f
Mohammadreza Khalilikhorram <mhmmdrz1999@gmail.com>
2024-04-08 23:53:32 +0330

    update input.ts and MInput.vue dark mode

--- ui.config/input.ts
+++ ui.config/input.ts
@@ -1,6 +1,7 @@
 export default {
     wrapperClass: 'relative',
-    base: 'relative bg-white block w-full text-gray-600 disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none focus:border-gray-300 border',
+    base: 'relative bg-white block w-full text-gray-600 disabled:cursor-not-allowed disabled:opacity-75 focus:outline-none border ring-inset ring-2 focus:ring-2 focus:ring-medium-blue text-medium-blue',
+    dark: 'dark:bg-slate-800 dark:border-slate-900 dark:text-gray-400 dark:focus:ring-emerald-500 dark:ring-emerald-500',
     rounded: 'md:rounded-3xl rounded-2xl',
     placeholder: 'placeholder-gray-400',
     padding: 'py-3 md:pl-8 md:pr-15 pl-5 pr-12',
